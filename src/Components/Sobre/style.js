74c5b10115da3c1c698179fb247a4df79beee115
DanielNunesJr <juniordaniel55@outlook.com>
2024-11-18 10:21:04 -0300

    estilizando e colocando imagens do super desafio

---
+++ src/Components/Sobre/style.js
@@ -0,0 +1,33 @@
+import styled from "styled-components";
+
+export const Section = styled.section`
+ 
+  height: 80vh;
+  display: flex;
+  justify-content: space-around;
+  align-items: center;
+  color: #1e3932
+`;
+
+
+export const Div = styled.div`
+width: 30vh;
+
+
+h2{
+    font-size: 1.5rem;
+    font-weight: 600;
+    margin-bottom: 1rem;
+}
+`
+
+export const Button = styled.button`
+width: 15vh;
+height: 5vh;
+background-color: #00704a;
+color: white;
+border: none;
+border-radius: 10px;
+cursor: pointer;
+border-radius: 30px;
+`
